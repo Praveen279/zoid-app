1d243cba06043f9586cc15bffc3653a55b4c867b
Praveen Reddy <praveenreddy.peto@gmail.com>
2020-03-11 23:41:22 +0530

    Updated port no

--- card-form.js
+++ card-form.js
@@ -1,6 +1,6 @@
 const CardForm = zoid.create({
     tag: 'card-form',
-    url: 'http://localhost:3000/',
+    url: 'http://localhost:8001/',
     dimensions: {
         height: '200px',
         width: '400px'
